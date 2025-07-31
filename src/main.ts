955fb5173e0838b4af2ed433966217277e9525db
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2025-07-31 11:15:57 -1000

    feat: themes + small updates

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "./styles/variables.css"
-import "./styles/main.css"
+import "./styles/global.css"
 import AddonManager from "./core/manager";
 import { DemoAddon } from "./addons/demo/DemoAddon";
 import { ViewportAddon } from "./addons/viewport/ViewportAddon";
-import { loadInitialAddons } from "./ui/components/addonLoader/loader";
+import { loadInitialAddons } from "./ui/components/addonLoaderScreen/loader";
 
 const debug = true;
 
